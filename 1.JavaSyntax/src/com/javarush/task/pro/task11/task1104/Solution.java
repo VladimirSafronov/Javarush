e808f13e2623f7759deffde08d9fa860eb74a552
VladimirSafronov <safron5000@mail.ru>
2022-02-05 09:37:12 +0600

    use static methods

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -4,8 +4,6 @@ package com.javarush.task.pro.task11.task1104;
 Конвертер строки
 */
 
-import javax.sound.midi.Soundbank;
-
 public class Solution {
     public static void main(String[] args) {
         String string = "12.84";
