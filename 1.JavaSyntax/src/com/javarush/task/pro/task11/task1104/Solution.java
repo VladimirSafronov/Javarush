1c266cff1d4b8db78a526f5b1cdb9d3013d26fe3
VladimirSafronov <safron5000@mail.ru>
2022-02-05 09:39:57 +0600

    convert number to int

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
     public static void main(String[] args) {
         String string = "12.84";
         double number = Double.parseDouble(string);
-        number = Math.round(number);
-        System.out.println(number);
+        int num = (int)Math.round(number);
+        System.out.println(num);
     }
 }
