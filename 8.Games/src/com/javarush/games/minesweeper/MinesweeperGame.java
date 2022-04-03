b7219a1b200fe079e279dd4f6e304333f3a3375a
VladimirSafronov <safron5000@mail.ru>
2022-04-03 15:25:44 +0300

    painted all fields

---
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -0,0 +1,22 @@
+package com.javarush.games.minesweeper;
+
+import com.javarush.engine.cell.*;
+
+public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
+    private GameObject [][] gameField = new GameObject[SIDE][SIDE];
+
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int i = 0; i < gameField[0].length; i++) {
+            for (int j = 0; j < gameField[1].length; j++) {
+                gameField[i][j] = new GameObject(j, i);
+                setCellColor(i, j, Color.AQUA);
+            }
+        }
+    }
+}
