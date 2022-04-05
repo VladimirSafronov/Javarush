c2ea48f6200aef5edbc4c3a5d0e88bc1a16d7c7c
VladimirSafronov <safron5000@mail.ru>
2022-04-05 15:20:42 +0300

    found min and max elements in array

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1123/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task11.task1123;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair result = getMinimumMaximumPair(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair getMinimumMaximumPair(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair(null, null);
+        }
+
+        return new Pair(getMin(inputArray), getMax(inputArray));
+    }
+
+    public static int getMin (int[] array) {
+        int min = array[0];
+        for (int value : array) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public static int getMax (int[] array) {
+        int max = array[0];
+        for (int value : array) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public static class Pair {
+        public Integer x;
+        public Integer y;
+
+        public Pair(Integer x, Integer y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
