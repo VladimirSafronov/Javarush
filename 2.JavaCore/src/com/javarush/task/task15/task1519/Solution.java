c903bcc3bf179afffcee441297393bd79b963975
VladimirSafronov <safron5000@mail.ru>
2022-05-05 17:57:43 +0600

    read String from keyboard and parsed their to different numbers

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1519/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task15.task1519;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Разные методы для разных типов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String str = reader.readLine();
+            while (!(str.equals("exit"))) {
+                if (str.contains(".")) {
+                    try {
+                        Double number = Double.parseDouble(str);
+                        Solution.print(number);
+                    } catch (NumberFormatException e) {
+                        Solution.print(str);
+                    }
+                }
+                else {
+                    try {
+                        int integer = Integer.parseInt(str);
+                        if (integer > 0 && integer < 128) {
+
+                            Solution.print((short)integer);
+                        }
+                        else {
+                            Solution.print(integer);
+                        }
+                    } catch (NumberFormatException e) {
+                        Solution.print(str);
+                    }
+                }
+                str = reader.readLine();
+            }
+        }
+    }
+
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+
+    public static void print(String value) {
+        System.out.println("Это тип String, значение " + value);
+    }
+
+    public static void print(short value) {
+        System.out.println("Это тип short, значение " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("Это тип Integer, значение " + value);
+    }
+}
