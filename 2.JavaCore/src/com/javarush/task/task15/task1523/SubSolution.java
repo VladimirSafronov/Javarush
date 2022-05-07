0e365d8ab9b368b5e2e60ea1f7321adf579f0077
VladimirSafronov <safron5000@mail.ru>
2022-05-07 10:32:27 +0600

    created constructors with all access modifier

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    protected SubSolution(int x) {
+        super(x);
+    }
+
+    SubSolution(String x) {
+        super(x);
+    }
+
+    public SubSolution(boolean x) {
+        super(x);
+    }
+}
