cffa321cae20cbb6f0a248fa10c406303c679317
VladimirSafronov <safron5000@mail.ru>
2022-07-06 16:58:23 +0300

    Serializabled Solution

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task20.task2014;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/* 
+Serializable Solution
+*/
+
+public class Solution implements Serializable {
+    public static void main(String[] args) {
+        String fileName = "/Users/vladimirsafronov/Desktop/file.txt";
+        Solution savedObject = new Solution(10);
+
+        try (FileOutputStream fos = new FileOutputStream(fileName);
+        ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(savedObject);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Solution loadedObject = new Solution(22);
+        try (FileInputStream fis = new FileInputStream(fileName);
+        ObjectInputStream ois = new ObjectInputStream(fis)) {
+            loadedObject = (Solution) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println((savedObject.string).equals(loadedObject.string));
+    }
+
+    private transient final String pattern = "dd MMMM yyyy, EEEE";
+    private transient Date currentDate;
+    private transient int temperature;
+    String string;
+
+    public Solution(int temperature) {
+        this.currentDate = new Date();
+        this.temperature = temperature;
+
+        string = "Today is %s, and the current temperature is %s C";
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        this.string = String.format(string, format.format(currentDate), temperature);
+    }
+
+    @Override
+    public String toString() {
+        return this.string;
+    }
+}
