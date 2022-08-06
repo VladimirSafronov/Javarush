c97db963d398fb1dae0a1644687cc1ae33a96ea2
VladimirSafronov <safron5000@mail.ru>
2022-08-06 10:17:28 +0300

    CustomTree (part4)

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -7,9 +7,32 @@ package com.javarush.task.task20.task2028;
 import java.io.Serializable;
 import java.util.AbstractList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+
+    Entry<String> root;
+    List<Entry<String>> list = new LinkedList<>();
+    int size = 0;
+
+    public CustomTree() {
+        this.root = new Entry<String>("0");
+        root.parent = root;
+        list.add(root.parent);
+    }
+
+    String getParent(String s) {
+        String parent = null;
+        for (int i = 0; i < list.size(); i++) {
+            if (s.equals(list.get(i).elementName)) {
+                parent = list.get(i).parent.elementName;
+                break;
+            }
+        }
+        return parent;
+    }
+
     @Override
     public String get(int index) {
         throw new UnsupportedOperationException();
@@ -47,7 +70,30 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public boolean add(String s) {
+        Entry<String> entry = new Entry<>(s);
+        for (int i = 0; i < list.size(); i++) {
+            entry.parent = list.get(i);
+            if (entry.parent.isAvailableToAddChildren()) {
+                list.add(entry);
+                size++;
+                if (entry.parent.availableToAddLeftChildren) {
+                    list.add(entry.parent);
+                    entry.parent.availableToAddLeftChildren = false;
+                    return true;
+                }
+                if (entry.parent.availableToAddRightChildren) {
+                    list.add(entry.parent);
+                    entry.parent.availableToAddRightChildren = false;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     static class Entry<T> implements Serializable {
