71dffbc75da1d4e1569714e0e8690ad52d395f30
VladimirSafronov <safron5000@mail.ru>
2022-08-07 17:59:13 +0300

    did generic method

--- 4.JavaCollections/src/com/javarush/task/task35/task3501/GenericStatic.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/GenericStatic.java
@@ -3,6 +3,6 @@ package com.javarush.task.task35.task3501;
 public class GenericStatic {
     public static <T> T someStaticMethod(T genericObject) {
         System.out.println(genericObject);
-        return genericObject;
+        return (T) genericObject;
     }
 }
