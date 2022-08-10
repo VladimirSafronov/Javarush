4427bbec970d5b56ce271c03d48ad7e3bfcddfb4
VladimirSafronov <safron5000@mail.ru>
2022-08-10 09:57:19 +0300

    Pattern Factory(2)

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,7 +1,15 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
 public class Solution {
     public static void main(String[] args) {
-
+        MaleFactory factory = new MaleFactory();
+        Human person1 = (Human) factory.getPerson(99);
+        System.out.println(person1);
+        Human person2 = (Human) factory.getPerson(4);
+        System.out.println(person2);
+        Human person3 = (Human) factory.getPerson(15);
+        System.out.println(person3);
     }
 }
