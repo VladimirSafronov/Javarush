0eabb92122a91aea2a2a5bafe36f4eccaed47142
VladimirSafronov <safron5000@mail.ru>
2022-08-18 13:10:48 +0300

    Aggregator 4. Created constant in HHStrategy.

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -3,5 +3,4 @@ package com.javarush.task.task28.task2810.model;
 public class HHStrategy implements Strategy {
 
     private static final String URL_FORMAT = "https://hh.ru/search/vacancy?text=java+%s&page=%d";
-
 }
