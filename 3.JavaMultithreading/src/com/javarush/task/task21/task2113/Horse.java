f015a6768cee5f7a0dfc0dce5a6fb962a26038ae
VladimirSafronov <safron5000@mail.ru>
2022-08-26 23:54:46 +0300

    Hippodrome(13)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -35,7 +35,15 @@ public class Horse {
         this.distance = distance;
     }
 
-    public void move() {}
+    public void move() {
+        distance += speed * Math.random();
+    }
 
-    public void print() {}
+    public void print() {
+        int counter = (int) distance;
+        for (int i = 0; i < counter; i++) {
+            System.out.print(".");
+        }
+        System.out.println(name);
+    }
 }
