634d04bd6e665b5c5207f3dd874bbeafb507dbb8
VladimirSafronov <safron5000@mail.ru>
2023-02-02 16:34:59 +0600

    task 2028

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,119 +1,207 @@
 package com.javarush.task.task20.task2028;
 
+import java.io.Serializable;
+import java.util.*;
+
 /*
 Построй дерево(1)
 */
+public class CustomTree extends AbstractList<String> implements Serializable, Cloneable {
+    private static final String LEFT = "LEFT_CHILD";
+    private static final String RIGHT = "RIGHT_CHILD";
 
-import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-
-public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
-
-    Entry<String> root;
-    List<Entry<String>> list = new LinkedList<>();
-    int size = 0;
-
-    public CustomTree() {
-        this.root = new Entry<String>("0");
-        root.parent = root;
-        list.add(root.parent);
-    }
-
-    String getParent(String s) {
-        String parent = null;
-        for (int i = 0; i < list.size(); i++) {
-            if (s.equals(list.get(i).elementName)) {
-                parent = list.get(i).parent.elementName;
-                break;
-            }
-        }
-        return parent;
-    }
-
-    @Override
-    public String get(int index) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public String set(int index, String element) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void add(int index, String element) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public String remove(int index) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public List<String> subList(int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    protected void removeRange(int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends String> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean add(String s) {
-        Entry<String> entry = new Entry<>(s);
-        for (int i = 0; i < list.size(); i++) {
-            entry.parent = list.get(i);
-            if (entry.parent.isAvailableToAddChildren()) {
-                list.add(entry);
-                size++;
-                if (entry.parent.availableToAddLeftChildren) {
-                    list.add(entry.parent);
-                    entry.parent.availableToAddLeftChildren = false;
-                    return true;
-                }
-                if (entry.parent.availableToAddRightChildren) {
-                    list.add(entry.parent);
-                    entry.parent.availableToAddRightChildren = false;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+    Entry<String> root = new Entry<>("Root");
 
     static class Entry<T> implements Serializable {
-
         String elementName;
-        boolean availableToAddLeftChildren;
-        boolean availableToAddRightChildren;
-        Entry<T> parent;
-        Entry<T> leftChild;
-        Entry<T> rightChild;
+        int lineNumber;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
 
         public Entry(String elementName) {
             this.elementName = elementName;
-            availableToAddLeftChildren = true;
-            availableToAddRightChildren = true;
+            availableToAddLeftChildren = availableToAddRightChildren = true;
+        }
+
+        public void checkChildren() {
+            if (leftChild != null)
+                availableToAddLeftChildren = false;
+            if (rightChild != null)
+                availableToAddRightChildren = false;
         }
 
         public boolean isAvailableToAddChildren() {
             return availableToAddLeftChildren || availableToAddRightChildren;
         }
+    }
 
+    @Override
+    public boolean remove(Object o) {
+        if (!o.getClass().getSimpleName().equals("String"))
+            throw new UnsupportedOperationException("Можно добавлять только строки.");
+
+        Queue<Entry<String>> nodes = new LinkedList<>(Collections.singletonList(root));
+
+        while (!nodes.isEmpty()) {
+            Entry<String> node = nodes.poll();
+
+            if (node.elementName.equals(o)) {
+                if (node.parent.leftChild == node) {
+                    node.parent.leftChild = null;
+                    node.parent.availableToAddLeftChildren = true;
+                }
+
+                if (node.parent.rightChild == node) {
+                    node.parent.rightChild = null;
+                    node.parent.availableToAddRightChildren = true;
+                }
+
+                // restoreCorruptedNodes(); // если обрублены все узлы, освобождает нижний уровень для добавления новых элементов.
+
+                return true;
+            }
+
+            if (node.leftChild != null)
+                nodes.offer(node.leftChild);
+            if (node.rightChild != null)
+                nodes.offer(node.rightChild);
+        }
+
+        return false;
+    }
+
+    @Override
+    public int size() {
+        int count = -1;
+
+        Queue<Entry<String>> nodes = new LinkedList<>(Collections.singletonList(root));
+
+        while (!nodes.isEmpty()) {
+            Entry<String> node = nodes.poll();
+            count++;
+
+            if (node.leftChild != null)
+                nodes.offer(node.leftChild);
+            if (node.rightChild != null)
+                nodes.offer(node.rightChild);
+        }
+
+        return count;
+    }
+
+    @Override
+    public boolean add(String s) {
+        Queue<Entry<String>> nodes = new LinkedList<>(Collections.singletonList(root));
+
+        while (!nodes.isEmpty()) {
+            Entry<String> currentNode = nodes.poll();
+
+            if (currentNode.isAvailableToAddChildren()) {
+                if (currentNode.availableToAddLeftChildren)
+                    return appendChild(s, currentNode, LEFT);
+                if (currentNode.availableToAddRightChildren)
+                    return appendChild(s, currentNode, RIGHT);
+            } else {
+                if (currentNode.leftChild != null)
+                    nodes.offer(currentNode.leftChild);
+                if (currentNode.rightChild != null)
+                    nodes.offer(currentNode.rightChild);
+            }
+        }
+
+        return false;
+    }
+
+    private boolean appendChild(String s, Entry<String> node, final String child) {
+        switch (child) {
+            case LEFT: {
+                node.leftChild = new Entry<>(s);
+                node.leftChild.parent = node;
+                node.checkChildren();
+                return true;
+            }
+
+            case RIGHT: {
+                node.rightChild = new Entry<>(s);
+                node.rightChild.parent = node;
+                node.checkChildren();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public String getParent(String elementName) {
+        Queue<Entry<String>> nodes = new LinkedList<>(Collections.singletonList(root));
+
+        while (!nodes.isEmpty()) {
+            Entry<String> node = nodes.poll();
+
+            if (node.elementName.equals(elementName))
+                return node.parent.elementName;
+
+            if (node.leftChild != null)
+                nodes.offer(node.leftChild);
+            if (node.rightChild != null)
+                nodes.offer(node.rightChild);
+        }
+
+        return null;
+    }
+
+    @Override
+    public String get(int index) {
+        throw new UnsupportedOperationException("Операция не поддерживается данным классом!");
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException("Операция не поддерживается данным классом!");
+    }
+
+    @Override
+    public void add(int index, String element) {
+        throw new UnsupportedOperationException("Операция не поддерживается данным классом!");
+    }
+
+    @Override
+    public String remove(int index) {
+        throw new UnsupportedOperationException("Операция не поддерживается данным классом!");
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException("Операция не поддерживается данным классом!");
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException("Операция не поддерживается данным классом!");
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw new UnsupportedOperationException("Операция не поддерживается данным классом!");
+    }
+
+    @Override
+    public String toString() {
+        Queue<Entry<String>> nodes = new LinkedList<>(Collections.singletonList(root));
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (!nodes.isEmpty()) {
+            Entry<String> entry = nodes.poll();
+
+            stringBuilder.append(entry.elementName).append(" -> ");
+
+            if (entry.leftChild != null)
+                nodes.offer(entry.leftChild);
+            if (entry.rightChild != null)
+                nodes.offer(entry.rightChild);
+        }
+
+        return stringBuilder.toString();
     }
 }
