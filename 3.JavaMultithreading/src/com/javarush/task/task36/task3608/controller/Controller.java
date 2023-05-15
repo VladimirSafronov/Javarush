2116d59fb916ed650557d10003fd09c2fcc22f2c
VladimirSafronov <safron5000@mail.ru>
2023-05-15 17:30:10 +0300

    Create EditUserView

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,11 +1,12 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
-import com.javarush.task.task36.task3608.view.UsersView;
+import com.javarush.task.task36.task3608.view.*;
 
 public class Controller {
     private Model model;
     private UsersView usersView;
+    private EditUserView editUserView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -13,6 +14,10 @@ public class Controller {
 
     public void setUsersView(UsersView usersView) {
         this.usersView = usersView;
+    }
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
     }
 
     public void onShowAllUsers() {
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -5,7 +5,17 @@ import com.javarush.task.task36.task3608.bean.User;
 import java.util.List;
 
 public class ModelData {
+    private User activeUser;
     private List<User> users;
+    private boolean displayDeletedUserList;
+
+    public User getActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(User activeUser) {
+        this.activeUser = activeUser;
+    }
 
     public List<User> getUsers() {
         return users;
@@ -13,5 +23,13 @@ public class ModelData {
 
     public void setUsers(List<User> users) {
         this.users = users;
+    }
+
+    public boolean isDisplayDeletedUserList() {
+        return displayDeletedUserList;
+    }
+
+    public void setDisplayDeletedUserList(boolean displayDeletedUserList) {
+        this.displayDeletedUserList = displayDeletedUserList;
     }
 }
