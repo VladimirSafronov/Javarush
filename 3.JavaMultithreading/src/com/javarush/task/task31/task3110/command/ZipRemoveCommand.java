26dfbd39a4a985e1fc0ab5098ec93bf8afd2956f
VladimirSafronov <safron5000@mail.ru>
2023-06-18 19:19:33 +0300

    Implement remove files from archive

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
@@ -1,8 +1,15 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Paths;
+
 public class ZipRemoveCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Введите название файла для удаления: ");
+        zipFileManager.removeFile(Paths.get(ConsoleHelper.readString()));
     }
 }
