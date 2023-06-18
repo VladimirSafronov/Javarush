8d8d2979194dab5479d077ffe50a282b8ba28294
VladimirSafronov <safron5000@mail.ru>
2023-06-18 20:45:06 +0300

    Implement add files to archive

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
@@ -1,8 +1,23 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        Path fileName = null;
+        try {
+            ConsoleHelper.writeMessage("Добавление файла в архив.");
+            ZipFileManager manager = getZipFileManager();
+            fileName = Paths.get(ConsoleHelper.readString());
+            manager.addFile(fileName);
+        } catch (PathIsNotFoundException ex) {
+            ConsoleHelper.writeMessage("Данного файла не существует " + fileName);
+        }
     }
 }
