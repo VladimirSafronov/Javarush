cc1996ea1c2ac3e865e28ef597c0586362d4b051
VladimirSafronov <safron5000@mail.ru>
2023-06-21 12:43:51 +0300

    Add some methods to Controller, View

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -13,7 +13,19 @@ public class Controller {
         this.view = view;
     }
 
+    public void init() {
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
 
     }
 }
