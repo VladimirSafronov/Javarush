c910f5aaac9c92fceab2440de0172a5518e832ba
Vladimir <89627108+VladimirSafronov@users.noreply.github.com>
2023-06-23 20:06:08 +0300

    Add videos in AdvertisementStorage

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -2,10 +2,18 @@ package com.javarush.task.task27.task2712.ad;
 
 //Хранилище рекламных роликов
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage {
     private static volatile AdvertisementStorage instance;
+    private final List<Advertisement> videos = new ArrayList<>();
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60));
+        add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60));
+        add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));
     }
 
     public static AdvertisementStorage getInstance() {
@@ -18,4 +26,13 @@ public class AdvertisementStorage {
         }
         return instance;
     }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
+    }
+
 }
