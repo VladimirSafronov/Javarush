0aeffca3ff9b93f7ed8a86e682c2cd9cbaab57f6
Vladimir <89627108+VladimirSafronov@users.noreply.github.com>
2023-06-23 17:45:47 +0300

    Create Cook
    * Implement pattern Observer

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,11 +1,14 @@
 package com.javarush.task.task27.task2712;
 
+
+import com.javarush.task.task27.task2712.kitchen.Cook;
+
 public class Restaurant {
     public static void main(String[] args) {
+        Cook cooker = new Cook("Cooker");
         Tablet tablet = new Tablet(1);
+        tablet.addObserver(cooker);
         tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
+
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -3,6 +3,7 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.SEVERE;
@@ -10,7 +11,7 @@ import static java.util.logging.Level.SEVERE;
 /**
  * Планшет клиента
  */
-public class Tablet {
+public class Tablet extends Observable {
     private final int number; //номер планшета
     public static Logger logger = Logger.getLogger(Tablet.class.getName());
 
@@ -21,13 +22,18 @@ public class Tablet {
     /**
      * будет создавать заказ из тех блюд, которые выберет пользователь
      */
-    public void createOrder() {
+    public Order createOrder() {
+        Order order = null;
         try {
-            Order order = new Order(this);
+            order = new Order(this);
             ConsoleHelper.writeMessage(order.toString());
         } catch (IOException e) {
             logger.log(SEVERE, "Console is unavailable.");
+        } finally {
+            setChanged();
+            notifyObservers(order);
         }
+        return order;
     }
 
     @Override
