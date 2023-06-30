e0182ce8d5ef224c9c81b8fa41f0277ff6f12dff
Vladimir <89627108+VladimirSafronov@users.noreply.github.com>
2023-06-30 12:30:41 +0300

    Create DirectorTablet

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/event/EventDataRow.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/event/EventDataRow.java
@@ -1,9 +1,13 @@
 package com.javarush.task.task27.task2712.statistic.event;
 
+import java.util.Date;
+
 /*
 Интерфейс-маркер. По нему определяем является ли переданный объект событием.
  */
 public interface EventDataRow {
 
     EventType getType();
+    Date getDate();
+    int getTime();
 }
