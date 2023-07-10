392f61bbe98efef36d814b748a7620d15d77ef97
Vladimir <89627108+VladimirSafronov@users.noreply.github.com>
2023-07-10 17:57:50 +0300

    Create field game in Tetris

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -3,6 +3,7 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
     private Field field;
     private Figure figure;
+    static Tetris game;
 
     public Field getField() {
         return field;
@@ -23,6 +24,7 @@ public class Tetris {
     public void step() {}
 
     public static void main(String[] args) {
-        
+        game = new Tetris();
+        game.run();
     }
 }
