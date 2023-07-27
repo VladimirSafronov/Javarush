bffa68f524051e982ab5150479994cbdcf40d1bf
Vladimir <89627108+VladimirSafronov@users.noreply.github.com>
2023-07-27 16:49:11 +0300

    Implement methods in Shortener

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,12 +1,27 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 public class Shortener {
-    public Long getId(String string) {
-        return null;
+    private Long lastId = 0L; //отвечает за последнее значение идентификатора, которое было использовано при добавлении новой строки в хранилище
+
+    private StorageStrategy storageStrategy; //хранит стратегию хранения данных
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id) {
-        return null;
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        }
+        lastId++;
+        storageStrategy.put(lastId, string);
+        return lastId;
+    }
+
+    public synchronized String getString(Long id) {
+        return storageStrategy.getValue(id);
     }
 
 }
