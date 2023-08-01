d8d77affe17d9275d8aec479840398a8f3867be6
Vladimir <89627108+VladimirSafronov@users.noreply.github.com>
2023-08-01 19:36:33 +0300

    Implement own HashMap

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task33.task3310;
 
-import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
-import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+import com.javarush.task.task33.task3310.strategy.*;
 
 import java.util.Date;
 import java.util.HashSet;
@@ -11,6 +10,9 @@ public class Solution {
 
     public static void main(String[] args) {
         testStrategy(new HashMapStorageStrategy(), 10000);
+        testStrategy(new OurHashMapStorageStrategy(), 10000);
+        testStrategy(new FileStorageStrategy(), 500);
+        testStrategy(new OurHashBiMapStorageStrategy(), 10000);
     }
 
     public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
